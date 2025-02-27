8a91ed14edb2699199fd7ef2fa30059b139aecf2
MrWaffles15 <163484569+MrWaffles15@users.noreply.github.com>
2025-02-27 09:26:55 -0800

    Update code.js

--- code.js
+++ code.js
@@ -1,3 +1,4 @@
 function loadiframe(){
-  document.getElementById("gameviewer").url = gamelist[0].embed;
+  document.getElementById('gameviewer').url = gamelist[0].embed;
+  document.getElementById('gameviewer').src += '';
 }
