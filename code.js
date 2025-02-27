b4f71ba893d429381fd1697e6adb85eb86e5d664
MrWaffles15 <163484569+MrWaffles15@users.noreply.github.com>
2025-02-27 13:12:19 -0800

    Update code.js

--- code.js
+++ code.js
@@ -2,6 +2,12 @@ function loadiframe(){
   document.getElementById('gameviewer').src = gamelist[0].embed;
 }
 
+function makebuttons(){
+  const newButton = document.createElement('button');
+  newButton.textContent = 'Click me!';
+  document.body.appendChild(newButton);
+}
+
 function update(){
   document.getElementById('gameviewer').src = gamelist[1].embed;
   document.getElementById('iframeid').src = document.getElementById('iframeid').src;
