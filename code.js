571f028981d0d037c23cb81d4360428931616846
MrWaffles15 <163484569+MrWaffles15@users.noreply.github.com>
2025-02-28 11:28:22 -0800

    Update code.js

--- code.js
+++ code.js
@@ -11,7 +11,8 @@ function makebuttons(){
   for (let i = 0; i < gamelist.length; i++) {
     newButton = document.createElement('button');
     newButton.textContent = "loadiframe("+i+")";//gamelist[i].title;
-    newButton.onclick = "loadiframe("+i+")";
+    newButton.id = "button"+i;
     document.body.appendChild(newButton);
+    document.getElementById('button'+i).onclick = "loadiframe("+i+")";
   }
 }
