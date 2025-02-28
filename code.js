f11be03fc1cb0476fd646f58c2934a32ecc62c9e
MrWaffles15 <163484569+MrWaffles15@users.noreply.github.com>
2025-02-28 09:59:00 -0800

    Update code.js

--- code.js
+++ code.js
@@ -10,7 +10,7 @@ function loadiframe(i){
 function makebuttons(){
   for (let i = 0; i < gamelist.length; i++) {
     newButton = document.createElement('button');
-    newButton.textContent = gamelist[i].title;
+    newButton.textContent = "loadiframe("+i+")";//gamelist[i].title;
     newButton.onclick = "loadiframe("+i+")";
     document.body.appendChild(newButton);
   }
