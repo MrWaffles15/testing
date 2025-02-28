4f6454b282b7f1168c11592fa84244dfd38d7c90
MrWaffles15 <163484569+MrWaffles15@users.noreply.github.com>
2025-02-28 08:45:58 -0800

    Update code.js

--- code.js
+++ code.js
@@ -2,6 +2,8 @@ var newButton;
 
 function loadiframe(b){
   document.getElementById('gameviewer').src = gamelist[b].embed;
+  document.getElementById('gameviewer').width = 1000;
+  document.getElementById('gameviewer').height = 600
 }
 
 function makebuttons(){
