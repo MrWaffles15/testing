058daf292a7e08e9f71d2a15bec3de3e6344d3e7
MrWaffles15 <163484569+MrWaffles15@users.noreply.github.com>
2025-02-28 13:10:42 -0800

    Update code.js

--- code.js
+++ code.js
@@ -4,6 +4,7 @@ function loadiframe(i){
   document.getElementById('gameviewer').width = 1000;
   document.getElementById('gameviewer').height = 600;
   document.getElementById('gameviewer').src = gamelist[i].embed;
+  document.getElementById('description').textContent = gamelist[i].description;
   document.getElementById('gameviewer').src = document.getElementById('gameviewer').src;
 }
 
