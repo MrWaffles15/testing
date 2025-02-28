2704f53138226f86100d700597734cd08f364cc7
MrWaffles15 <163484569+MrWaffles15@users.noreply.github.com>
2025-02-28 09:20:10 -0800

    Update code.js

--- code.js
+++ code.js
@@ -1,9 +1,9 @@
 var newButton;
 
-function loadiframe(b){
+function loadiframe(){
   document.getElementById('gameviewer').width = 1000;
   document.getElementById('gameviewer').height = 600;
-  document.getElementById('gameviewer').src = gamelist[b].embed;
+  document.getElementById('gameviewer').src = gamelist[0].embed;
   document.getElementById('gameviewer').src = document.getElementById('gameviewer').src;
 }
 
@@ -11,7 +11,7 @@ function makebuttons(){
   for (let i = 0; i < gamelist.length; i++) {
     newButton = document.createElement('button');
     newButton.textContent = gamelist[i].title;
-    newButton.onclick = "loadiframe("+i+")";
+    newButton.onclick = "loadiframe()";
     document.body.appendChild(newButton);
   }
 }
